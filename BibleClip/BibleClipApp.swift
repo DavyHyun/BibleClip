8a5ec123d0801f28aa289605381bf6f6a9100e94
Davyhyun <dhyun1124@gmail.com>
2024-06-22 13:48:06 -0700

    Initial Commit

---
+++ BibleClip/BibleClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleClipApp.swift
+//  BibleClip
+//
+//  Created by David Hyun on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BibleClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BibleClip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BibleClip
+//
+//  Created by David Hyun on 6/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
